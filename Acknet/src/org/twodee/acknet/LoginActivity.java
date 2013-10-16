637df2e7f4e79de522cc09ecd0f7970efe12168e
dbustosp <dbustos10@gmail.com>
2013-10-15 22:31:49 -0500

    Clean MainActividy.java

--- Acknet/src/org/twodee/acknet/LoginActivity.java
+++ Acknet/src/org/twodee/acknet/LoginActivity.java
@@ -1,13 +1,24 @@
 package org.twodee.acknet;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 	
@@ -15,13 +26,63 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_login);
-	}
+	
+	
+	    final Button loginButton = (Button) findViewById(R.id.login_2);
+	    final EditText login_username = (EditText)findViewById(R.id.username);
+	    final EditText login_password = (EditText)findViewById(R.id.password);
+	    
+	    loginButton.setOnClickListener(new OnClickListener(){
 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    // Inflate the menu; this adds items to the action bar if it is present.
-	    getMenuInflater().inflate(R.menu.main, menu);
-	    return true;
+			@Override
+			public void onClick(View arg0) {
+				
+				System.out.println("EditText username: " + login_username.getText().toString());
+				System.out.println("EditText username: " + login_password.getText().toString());
+				
+				//int j = postData(login_username.getText().toString(),login_password.getText().toString());
+								
+			}
+        });
+	    
+	    
+	
+	
+	
 	}
+	
+	public int postData(String username, String password){
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("http://arcane-crag-4782.herokuapp.com/user/" + username);
+		
+		try {
+	        // Add your data
+	        
+			List<NameValuePair> data_login = new ArrayList<NameValuePair>(2);
+	        
+			data_login.add(new BasicNameValuePair("username", username));
+			data_login.add(new BasicNameValuePair("password", password));
+	        
+	        httppost.setEntity(new UrlEncodedFormEntity(data_login));
+
+	        // Execute HTTP Post Request
+	        HttpResponse response = httpclient.execute(httppost);
+	        
+	        System.out.println("Response: " + response);
+	        
+	        
+	    } catch (ClientProtocolException e) {
+	        // TODO Auto-generated catch block
+	    	System.out.println("ClientProtocolException");
+	    } catch (IOException e) {
+	    	System.out.println("IOEXCEPTION");
+	    }
+		
+		
+		return 0;
+	}
+	
+	
+	
 }
--- Acknet/src/org/twodee/acknet/MainActivity.java
+++ Acknet/src/org/twodee/acknet/MainActivity.java
@@ -3,10 +3,8 @@ package org.twodee.acknet;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -16,27 +14,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        final Button login = (Button) findViewById(R.id.login);
+        final Button signupButton = (Button) findViewById(R.id.signup);
         
-        login.setOnTouchListener(new OnTouchListener(){
-
-        	@Override
-			public boolean onTouch(View arg0, MotionEvent arg1) {
-        		 Intent i = new Intent(MainActivity.this, LoginActivity.class);     
-                 startActivity(i);
-                 finish(); //should use the finish if you need to preserve memory
-                           //other wise don't use it.
-				return false;
-			}
+        signupButton.setOnClickListener(new OnClickListener() {
+	        @Override
+	        public void onClick(View view) {
+	        	
+	        	Intent myIntent = new Intent(view.getContext(), LoginActivity.class); /** Class name here */
+	        	startActivityForResult(myIntent, 0);
+	        	
+	        }
 	    });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }
