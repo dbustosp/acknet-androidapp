04bea63ea3752fb0526e798164ee45a4e9cf11e9
dbustosp <dbustos10@gmail.com>
2013-10-22 18:47:34 -0500

    Added Singleton for Connection

--- Acknet/src/org/twodee/acknet/Connection.java
+++ Acknet/src/org/twodee/acknet/Connection.java
@@ -9,7 +9,7 @@ public final class Connection {
 	
 	
 	private Connection(){
-		IP = "http://137.28.129.21:3000";	
+		IP = "http://137.28.230.99:3000";	
 	}
 	
 	public static Connection getInstance(){
